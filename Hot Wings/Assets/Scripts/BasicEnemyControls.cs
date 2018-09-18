423f4b0d4bca28c310c6ce2d75d1cf3c18a7c3d5
Jake Velicer <007velicer@gmail.com>
2018-09-18 15:28:14 -0700

    Comments
    Added Comments

--- Hot Wings/Assets/Scripts/BasicEnemyControls.cs
+++ Hot Wings/Assets/Scripts/BasicEnemyControls.cs
@@ -14,6 +14,7 @@ public class BasicEnemyControls : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
+		// Gets the Rigidbody of the game object this script is on
 		Rigidbody = GetComponent<Rigidbody2D> ();
 		
 	}
@@ -21,16 +22,21 @@ public class BasicEnemyControls : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		// Finds the Player's transform and stores it in target
 		Target = GameObject.FindGameObjectWithTag ("Player").transform;
+
 		Movement();
 		ChaseTarget();
 		
 	}
 
+	// Controls the actual moevment of the object
 	void Movement () {
 
+		// Checks if it is allowed to chase the player
 		if (CanChase == true) {
 
+			// Pushes the enemy in a direction based upon which side the player is on
 			if (ToTheRight == false) {
 			Rigidbody.AddForce (MovementSpeed * new Vector2 (-1,0));
 			}
@@ -41,6 +47,7 @@ public class BasicEnemyControls : MonoBehaviour {
 		}
 	}
 
+	// Determines if the range of the player is close enough to be chased
 	void ChaseTarget () {
 
 		if (Vector3.Distance(Target.position, transform.position) <= ChaseRange) {
@@ -51,6 +58,7 @@ public class BasicEnemyControls : MonoBehaviour {
 
 	}
 
+	// Determines the direction the object faces when chasing
 	void ChaseDirection () {
 
 		if (Target.position.x > transform.position.x + 1) {
