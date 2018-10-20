dad50324fff1771a65eb9d168599cd0eedb62b72
Jake Velicer <007velicer@gmail.com>
2018-10-19 17:58:26 -0700

    Pepper Bush started and Pepper Art added

---
+++ Hot Wings/Assets/Scripts/BushBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BushBehavior : MonoBehaviour {
+
+	public GameObject[] PepperDrops;
+	private GameObject ChosenPepper;
+
+	// Use this for initialization
+	void Start () {
+
+		ChosenPepper = PepperDrops[Random.Range(0,6)];
+
+		GameObject shot = Instantiate(ChosenPepper, transform.position + new Vector3 (0, 0, 0),
+		Quaternion.identity) as GameObject;
+		shot.GetComponent<SpriteRenderer>().sortingLayerName = "Player";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
