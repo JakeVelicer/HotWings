df1687e4f1d118db87836b7c309c99106d2f699d
Jake Velicer <007velicer@gmail.com>
2018-11-12 14:18:05 -0800

    Lightning Bolt Animations Implemented

--- Hot Wings/Assets/Scripts/BasicBullet.cs
+++ Hot Wings/Assets/Scripts/BasicBullet.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class BasicBullet : MonoBehaviour {
 
+	public Animator Anim;
+
 	// Use this for initialization
 	void Start () {
+
+		if (gameObject.name == "LightningBullet(Clone)") {
+			Anim.Play("Shock1");
+		}
+		else if (gameObject.name == "LightningBullet2(Clone)") {
+			Anim.Play("Shock2");
+		}
+		else if (gameObject.name == "LightningBullet3(Clone)") {
+			Anim.Play("Shock3");
+		}
+		else if (gameObject.name == "LightningBullet4(Clone)") {
+			Anim.Play("Shock4");
+		}
 		Destroy (gameObject, 1.3f);
+		
 	}
 
 	void OnTriggerEnter2D(Collider2D collision) {
