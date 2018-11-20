ebf44667377fd47b1468666ed571fb13a94b8955
Jake Velicer <007velicer@gmail.com>
2018-11-19 17:10:50 -0800

    Brick Destroys itself
    Yes, that warranted it’s own push.

---
+++ Hot Wings/Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour {
+
+	public int Timer;
+
+	// Use this for initialization
+	void Start () {
+
+		Destroy(gameObject, Timer);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
