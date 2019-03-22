3812700214f6a52ee400e1bc6e5831a5a8c1485e
Jake Velicer <007velicer@gmail.com>
2019-03-22 00:37:51 -0500

    More attempts for button selection

---
+++ Hot Wings/Assets/Scripts/GUIControllerStart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GUIControllerStart : MonoBehaviour {
+
+	public Button BackButtonHelp;
+	public Button BackButtonCredits;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadByIndex(int sceneIndex) {
+        SceneManager.LoadScene (sceneIndex);
+    }
+
+	public void QuitGame() {
+		Application.Quit();
+		Debug.Log("Game Quit Called");
+	}
+
+	public void HelpClicked() {
+		BackButtonHelp.Select();
+	}
+
+	public void CreditsClicked() {
+		BackButtonCredits.Select();
+	}
+}
