cdb66204ac98351c8e432597b801be2eb0c98a49
Jake Velicer <007velicer@gmail.com>
2019-04-12 15:10:05 -0500

    Controller layout popup added

---
+++ Hot Wings/Assets/SwitchShownInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchShownInput : MonoBehaviour {
+
+	public GameObject controlsPC;
+	public GameObject controlsController;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetAxisRaw("Horizontal") < 0){
+			controlsController.SetActive(true);
+			controlsPC.SetActive(false);
+		}
+		else if(Input.GetAxisRaw("Horizontal") > 0){
+			controlsPC.SetActive(true);
+			controlsController.SetActive(false);
+		}
+		
+	}
+}
