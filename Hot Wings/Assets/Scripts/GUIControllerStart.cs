e7c21a45c0d31e8b590f126b02814ae27c1d89ac
JakeVelicer <007velicer@gmail.com>
2020-06-01 17:57:33 -0500

    Updated Main Menu To Fit In Phone Screen

--- Hot Wings/Assets/Scripts/GUIControllerStart.cs
+++ Hot Wings/Assets/Scripts/GUIControllerStart.cs
@@ -9,35 +9,58 @@ public class GUIControllerStart : MonoBehaviour {
 	public Button StartButton;
 	public Button BackButtonHelp;
 	public Button BackButtonCredits;
+	public GameObject QuitButton;
 
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Start ()
+	{
+		if (Application.platform == RuntimePlatform.IPhonePlayer
+		|| Application.platform == RuntimePlatform.Android)
+		{
+			QuitButton.SetActive(false);
+		}
+		else
+		{
+			StartButton.Select();
+			QuitButton.SetActive(true);
+		}
 	}
 
-    public void LoadByIndex(int sceneIndex) {
-        SceneManager.LoadScene (sceneIndex);
+    public void LoadByIndex(int sceneIndex)
+	{
+        SceneManager.LoadScene(sceneIndex);
     }
 
-	public void QuitGame() {
+	public void QuitGame()
+	{
 		Application.Quit();
 		Debug.Log("Game Quit Called");
 	}
 
-	public void HelpClicked() {
-		BackButtonHelp.Select();
+	public void HelpClicked()
+	{
+		if (Application.platform != RuntimePlatform.IPhonePlayer
+		&& Application.platform != RuntimePlatform.Android)
+		{
+			BackButtonHelp.Select();
+		}
 	}
 
-	public void CreditsClicked() {
-		BackButtonCredits.Select();
+	public void CreditsClicked()
+	{
+		if (Application.platform != RuntimePlatform.IPhonePlayer
+		&& Application.platform != RuntimePlatform.Android)
+		{
+			BackButtonCredits.Select();
+		}
 	}
 
-	public void BackClicked() {
-		StartButton.Select();
+	public void BackClicked()
+	{
+		if (Application.platform != RuntimePlatform.IPhonePlayer
+		&& Application.platform != RuntimePlatform.Android)
+		{
+			StartButton.Select();
+		}
 	}
 }
--- Hot Wings/Assets/Scripts/SwitchShownInput.cs
+++ Hot Wings/Assets/Scripts/SwitchShownInput.cs
@@ -6,23 +6,36 @@ public class SwitchShownInput : MonoBehaviour {
 
 	public GameObject controlsPC;
 	public GameObject controlsController;
+	private bool onMobile;
 
 	// Use this for initialization
 	void Start () {
+
+		if (Application.platform == RuntimePlatform.IPhonePlayer
+		|| Application.platform == RuntimePlatform.Android)
+		{
+			onMobile = true;
+			controlsController.SetActive(false);
+			controlsPC.SetActive(false);
+		}
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
-		if(Input.GetAxisRaw("Horizontal") < 0){
-			controlsController.SetActive(true);
-			controlsPC.SetActive(false);
+	void Update ()
+	{
+		if (!onMobile)
+		{
+			if(Input.GetAxisRaw("Horizontal") < 0)
+			{
+				controlsController.SetActive(true);
+				controlsPC.SetActive(false);
+			}
+			else if(Input.GetAxisRaw("Horizontal") > 0)
+			{
+				controlsPC.SetActive(true);
+				controlsController.SetActive(false);
+			}
 		}
-		else if(Input.GetAxisRaw("Horizontal") > 0){
-			controlsPC.SetActive(true);
-			controlsController.SetActive(false);
-		}
-		
 	}
 }
