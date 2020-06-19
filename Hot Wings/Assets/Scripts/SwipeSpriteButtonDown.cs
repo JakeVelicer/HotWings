beaec61420554c400c07209c26a37e4130ab176d
JakeVelicer <007velicer@gmail.com>
2020-06-18 21:27:43 -0500

    End Screen Assets Reimplement

---
+++ Hot Wings/Assets/Scripts/SwipeSpriteButtonDown.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwipeSpriteButtonDown : MonoBehaviour
+{
+	public Sprite unselectedBox;
+	public Sprite selectedBox;
+	public Image inventoryBox2;
+	private bool onMobile;
+
+	private void Awake()
+	{
+		if (Application.platform == RuntimePlatform.IPhonePlayer
+		|| Application.platform == RuntimePlatform.Android)
+		{
+			onMobile = true;
+		}
+		else
+		{
+			onMobile = false;
+		}
+	}
+
+	public void SelectSprite()
+	{
+		if (onMobile)
+		inventoryBox2.sprite = selectedBox;
+	}
+
+	public void UnSelectSprite()
+	{
+		if (onMobile)
+		inventoryBox2.sprite = unselectedBox;
+	}
+
+}
