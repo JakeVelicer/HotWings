67537366e0da70bd0ad1f471b94b1b8b272e0e42
JakeVelicer <007velicer@gmail.com>
2020-06-26 00:53:51 -0500

    Added New Main Menu Backgrounds

---
+++ Hot Wings/Assets/Scripts/MainMenuButtonPositioner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuButtonPositioner : MonoBehaviour {
+
+	public float pcYPos;
+	public float mobileYPos;
+	public RectTransform buttonHolder;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		if (Application.platform == RuntimePlatform.IPhonePlayer
+		|| Application.platform == RuntimePlatform.Android)
+		{
+			buttonHolder.localPosition = new Vector3(0, mobileYPos, 1);
+		}
+		else
+		{
+			buttonHolder.localPosition = new Vector3(0, pcYPos, 1);
+		}
+	}
+	
+}
