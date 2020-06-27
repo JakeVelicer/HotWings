c5d3b3864ee6a589ab457fa25740a0d6da17d125
JakeVelicer <007velicer@gmail.com>
2020-06-26 20:12:13 -0500

    Cloud Fix, Sign Fix

--- Hot Wings/Assets/Scripts/CloudBehavior.cs
+++ Hot Wings/Assets/Scripts/CloudBehavior.cs
@@ -5,42 +5,22 @@ using UnityEngine;
 public class CloudBehavior : MonoBehaviour {
 	
 	public float speed;
-	public float waitToStart;
 	public Rigidbody2D rigidBody;
-	private Vector3 startPosition;
+	public float restartPositionX;
 	private float chosenSpeed;
-	private bool canMove;
-
-	void Start()
-	{
-		//chosenSpeed = Random.Range(speeds.x, speeds.y);
-		startPosition = transform.position;
-		StartCoroutine(WaitToMove());
-
-	}
-
-	private IEnumerator WaitToMove()
-	{
-		yield return new WaitForSeconds(waitToStart);
-		canMove = true;
-	}
 
 	// Update is called once per frame
 	void FixedUpdate()
 	{
-		if (canMove)
-		{
-			rigidBody.velocity = Vector2.left * speed;
-		}
+		rigidBody.velocity = Vector2.left * speed;
 	}
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
-		Debug.Log("colliding");
 		if (other.gameObject.CompareTag("CloudRestart"))
 		{
 			//chosenSpeed = Random.Range(speeds.x, speeds.y);
-			transform.position = startPosition;
+			transform.localPosition = new Vector3 (restartPositionX, transform.position.y, transform.position.z);
 		}
 	}
 }
